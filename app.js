10f01aa6f5fa683e85345a8aadb200fa0cefeb84
Rihab.oueslati <rihaboueslati@takiacademyteam.com>
2023-03-23 08:57:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const cors = require("cors");
+
+const filesPayloadedExists = require("./middleware/filesPayloadExists");
+const fileExtLimiter = require("./middleware/fileExtensionLimiter");
+const fileSizeLimiter = require("./middleware/fileSizeLimiter");
+
+const PORT = process.env.PORT || 3500;
+
+const app = express();
+
+app.use(cors("*"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post(
+  "/upload",
+  fileUpload({ createParentPath: true }),
+  filesPayloadedExists,
+  fileExtLimiter([".png", ".jpg", "jpeg"]),
+  fileSizeLimiter,
+  (req, res) => {
+    const files = req.files;
+    console.log(files);
+
+    Object.keys(files).forEach((key) => {
+      const filepath = path.join(__dirname, "files", files[key].name);
+      files[key].mv(filepath, (err) => {
+        if (err) return res.status(500).json({ status: "error", message: err });
+      });
+    });
+
+    return res.json({
+      status: "success",
+      message: Object.keys(files).toString(),
+    });
+  }
+);
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
---
+++ middleware/filesPayloadExists.js
@@ -0,0 +1,7 @@
+const filesPayloadedExists = (req, res, next) => {
+  if (!req.files)
+    return res.status(400).json({ status: "error", message: "missing files!" });
+  next();
+};
+
+module.exports = filesPayloadedExists;
